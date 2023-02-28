115d8c4ca0a0f1a11608c6078f92bf67d836e218
Sophia Sunkin <sophiasunkin@Sophias-MacBook-Pro.local>
2023-02-27 20:11:15 -0800

    finished version

--- App.js
+++ App.js
@@ -2,61 +2,41 @@ import React, {useState, useEffect} from 'react';
 import {StyleSheet, View, Text, Button, PermissionsAndroid} from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
 import { PERMISSIONS, check, request } from 'react-native-permissions'
-// Function to get permission for location
-const requestLocationPermission = async () => {
-  try {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      {
-        title: 'Geolocation Permission',
-        message: 'Can we access your location?',
-        buttonNeutral: 'Ask Me Later',
-        buttonNegative: 'Cancel',
-        buttonPositive: 'OK',
-      },
-    );
-    console.log('granted', granted);
-    if (granted === 'granted') {
-      console.log('You can use Geolocation');
-      return true;
-    } else {
-      console.log('You cannot use Geolocation');
-      return false;
-    }
-  } catch (err) {
-    return false;
-  }
-};
+
 const App = () => {
   // state to hold location
   const [location, setLocation] = useState(false);
-  // function to check permissions and get Location
+
   const getLocation = async () => {
-    const RESULTS = await check(PERMISSIONS.IOS.LOCATION);
-    RESULTS.then(res => {
-      console.log('res is:', res);
-      if (res === RESULTS.GRANTED) {
-        Geolocation.getCurrentPosition(
-          position => {
-            console.log(position);
-            setLocation(position);
-          },
-          error => {
-            // See error code charts below.
-            console.log(error.code, error.message);
-            setLocation(false);
-          },
-          {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
-        );
-      } else if (res === RESULTS.DENIED) {
-        const res2 = request(PERMISSIONS.IOS.LOCATION);
-        res2 === RESULTS.GRANTED 
-          ? setLocation(true)
-          : setLocation(false);
-      }
-    });
-    console.log(location);
+    const result_in_use = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    const result_always = await check(PERMISSIONS.IOS.LOCATION_ALWAYS)
+    
+    if (result_in_use === 'granted' || result_always === 'granted') {
+      Geolocation.getCurrentPosition(
+              position => {
+                console.log(position);
+                setLocation(position);
+              },
+              error => {
+                // See error code charts below.
+                console.log(error.code, error.message);
+                setLocation(false);
+              },
+              {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000}
+      );
+    } 
+    else {
+            const res2 = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+            console.log(res2);
+            if (res2 === 'granted') {
+              setLocation(true);
+              getLocation();
+            } else {
+              setLocation(false);
+            }
+    }
   };
+  
   return (
     <View style={styles.container}>
       <Text>Welcome!</Text>
